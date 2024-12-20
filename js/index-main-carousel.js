9a513596152783cb20e45418617b629fc197525c
JCS Java <jeronimo.calvosanchez@gmail.com>
2024-12-20 18:16:40 +0100

    Add vip clients

--- js/index-main-carousel.js
+++ js/index-main-carousel.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper-container', {
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        autoplay: {
-            delay: 10000,
-            disableOnInteraction: false,
-        },
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    let currentSlide = 0;
+
+    slides.forEach((slide, index) => {
+        slide.style.left = `${index * slideWidth}px`;
+    });
+
+    const moveToSlide = (currentIndex) => {
+        track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    };
+
+    nextButton.addEventListener('click', () => {
+        currentSlide = (currentSlide + 1) % slides.length;
+        moveToSlide(currentSlide);
+    });
+
+    prevButton.addEventListener('click', () => {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        moveToSlide(currentSlide);
     });
 });
